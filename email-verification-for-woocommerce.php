2a0138045ae7afa655499381d3a03ff2fd435577
kousikmukherjee <kousikmukherjee.li@gmail.com>
2024-09-05 14:09:59 +0530

    v2.8.8

--- email-verification-for-woocommerce.php
+++ email-verification-for-woocommerce.php
@@ -3,13 +3,13 @@
 Plugin Name: Email Verification for WooCommerce
 Plugin URI: https://wpfactory.com/item/email-verification-for-woocommerce/
 Description: Verify user emails in WooCommerce. Beautifully.
-Version: 2.8.7
+Version: 2.8.8
 Author: WPFactory
 Author URI: https://wpfactory.com
 Text Domain: emails-verification-for-woocommerce
 Domain Path: /langs
 Copyright: © 2024 WPFactory
-WC tested up to: 9.1
+WC tested up to: 9.2
 Requires Plugins: woocommerce
 License: GNU General Public License v3.0
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
@@ -63,7 +63,7 @@ final class Alg_WC_Email_Verification {
 	 * @var   string
 	 * @since 1.0.0
 	 */
-	public $version = '2.8.7';
+	public $version = '2.8.8';
 
 	/**
 	 * @var   Alg_WC_Email_Verification The single instance of the class
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: wpcodefactory, omardabbas, karzin, anbinder, algoritmika, kousikmu
 Tags: woocommerce, email, verification, email verification, woo commerce
 Requires at least: 4.4
 Tested up to: 6.6
-Stable tag: 2.8.7
+Stable tag: 2.8.8
 License: GNU General Public License v3.0
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -355,6 +355,8 @@ Once activated, access the plugin's settings by navigating to “WooCommerce > S
 
 
 == Changelog ==
+= 2.8.8 - 05/09/2024 =
+* WC tested up to: 9.2
 
 = 2.8.7 - 30/07/2024 =
 * WC tested up to: 9.1
